bf4a4ee30331590845ce8113dfe586a066f71292
Jan Kalęba <93594746+janekkodowanie@users.noreply.github.com>
2023-05-18 03:08:40 +0200

    MST -> Kruskal's & 2x Prim's algorithm.

--- Algorithms/Graphs/MST.py
+++ Algorithms/Graphs/MST.py
@@ -64,7 +64,7 @@ def relaxPrime(u: int, v: int, length: int, distance: list, parent: list, Q: Pri
 # relaxation function -> instead of the absolute distance
 # from start to current v, only current-edge distance
 # is being taken into account.
-def PrimAlgorithm(Graph: list[list[tuple[int, int]]]):
+def PrimAlgorithmReturningEdges(Graph: list[list[tuple[int, int]]]):
     n = len(Graph)
 
     MST = []
@@ -90,6 +90,37 @@ def PrimAlgorithm(Graph: list[list[tuple[int, int]]]):
         if not added[u]:
             MST.append((parent[u], u, du))
             added[u] = True
+
+    return MST
+
+def PrimAlgorithmReturningTree(Graph: list[list[tuple[int, int]]]):
+    n = len(Graph)
+
+    MST = [[] for _ in range(n)]
+
+    parent = [None for _ in range(n)]
+    distance = [inf for _ in range(n)]
+    visited = [False for _ in range(n)]
+    added = [False for _ in range(n)]
+
+    # random start vertex -> can be specified if wanted
+    v0 = randint(0, n - 1)
+
+    Q = PriorityQueue()
+    Q.put((0, v0))
+
+    while not Q.empty():
+        du, u = Q.get()
+
+        if not visited[u]:
+            for (v, l) in Graph[u]:
+                relaxPrime(u, v, l, distance, parent, Q)
+
+        if not added[u] and parent[u] is not None:
+            MST[parent[u]].append((u, du))
+            MST[u].append((parent[u], du))
+            added[u] = True
+            added[parent[u]] = True
 
     return MST
 
@@ -127,9 +158,15 @@ if __name__ == '__main__':
 
     print("\nPrim:")
     Gr = transform(Edges, N)
-    mst2 = PrimAlgorithm(Gr)
+    mst2 = PrimAlgorithmReturningEdges(Gr)
     for edge in mst2:
         print(edge[0], edge[1])
         primSum += edge[2]
 
+    print("\nPrim v2:")
+    mstTree = PrimAlgorithmReturningTree(Gr)
+    for row in mstTree:
+        print(row)
+    print()
+    
     print(f"KruskalSum: {kruskalSum} & PrimSum: {primSum}")
