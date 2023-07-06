29ca9a4f0d0e5fa2aad147c68664250cec535fd7
WegielWBucie <pondroszejset@gmail.com>
2023-07-06 03:23:05 +0200

    Edit Distance -> O(n^2).

--- BITAlgo/Repair/Repair.py
+++ BITAlgo/Repair/Repair.py
@@ -1,15 +1,29 @@
 from zad8ktesty import runtests
 
 
+# Edit Distance | DP Problem
+
 def napraw(s, t):
     n = len(t)
     m = len(s)
 
-    print(s, t)
+    # f(i, j) - liczba zmian potrzebnych do tego, aby z s[0:j + 1] było t[0:i + 1]
+    # ([0:k] pomija znak pod k-tym indeksem, stąd + 1)
 
-    # f
+    #     | e | x | a | m | p | l | e | S |
+    # - - - - - - - - - - - - - - - - - - -
+    # | e |
+    # - - -
+    # | g |              ...
+    # - - -
+    # | T |
+    # - - -
+
     f = [[0 for _ in range(m)] for __ in range(n)]
 
+    # Edge cases
+
+    # (0, 0)
     foundFirst = False
     if s[0] == t[0]:
         foundFirst = True
@@ -17,6 +31,7 @@ def napraw(s, t):
     else:
         f[0][0] = 1
 
+    # First row
     found = foundFirst
     for i in range(1, m):
         substitutionCost = 0
@@ -26,10 +41,11 @@ def napraw(s, t):
             found = True
         f[0][i] = f[0][i - 1] + substitutionCost
 
-    found = False
+    found = foundFirst
     if s[0] == t[0]:
         found = True
 
+    # First column
     for i in range(1, n):
         substitutionCost = 0
         if t[i] != s[0] or found:
@@ -38,6 +54,7 @@ def napraw(s, t):
             found = True
         f[i][0] = f[i - 1][0] + substitutionCost
 
+    # General case
     for i in range(1, n):
         for j in range(1, m):
             substitutionCost = 0
